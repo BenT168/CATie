c39847607a8391dcad439c3a0232942b61be9a7d
Harry Uglow <hu115@ic.ac.uk>
2017-05-31 12:33:30 +0100

    Edited urls.py to get server to run.

--- backend/ARi/ARi/urls.py
+++ backend/ARi/ARi/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
-
 urlpatterns = [
-    # Web App Entry
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^admin/', admin.site.urls),
 ]
