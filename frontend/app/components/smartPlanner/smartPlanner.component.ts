47ba7fa6b5cc106ec54f9534bdb087908339a512
Harry Uglow <hu115@ic.ac.uk>
2017-06-01 12:46:15 +0100

    Pushed new Clarity frontend for Josh to work on.

--- frontend/app/components/smartPlanner/smartPlanner.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'smart-planner',
-    templateUrl: `app/components/smartPlanner/smartPlanner.component.html`
-})
-
-export class SmartPlannerComponent {}
