3f47824d4bde3eeaf89d80bd42e1f4672cd6f6f1
Ruhi Choudhury <arc13@ic.ac.uk>
2017-06-02 08:01:12 +0100

    [RC] Attempt to add username to CalendarEvent model.
    Changes currenctly not reflected by Postgres DB. Investigation pending.

--- backend/ARi/planner/models.py
+++ backend/ARi/planner/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class CalendarEvent(models.Model):
-    username    = models.CharField(max_length=10)
+    username = models.CharField(max_length=10)
     title       = models.CharField(max_length=60)
     date        = models.DateField()
     startTime   = models.TimeField()
