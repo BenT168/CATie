6adedc22d53eebdc7f93492c8cd815b35f2238b5
Ruhi Choudhury <arc13@ic.ac.uk>
2017-06-02 07:59:21 +0100

    [RC] Began calendar component.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
@@ -10,18 +10,22 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { CalendarModule } from 'angular-calendar';
 
+import { CalendarComponent } from './calendar/calendar.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
         AboutComponent,
-        HomeComponent
+        HomeComponent,
+        CalendarComponent
     ],
     imports: [
         BrowserAnimationsModule,
         BrowserModule,
         FormsModule,
         HttpModule,
+        JsonpModule,
         ClarityModule.forRoot(),
         CalendarModule.forRoot(),
         ROUTING
