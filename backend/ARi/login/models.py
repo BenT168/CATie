c7ccd4f794805e575517ac08badeb19c7bcdd3aa
Harry Uglow <hu115@ic.ac.uk>
2017-06-06 12:21:54 +0100

    Working on login backend.

---
+++ backend/ARi/login/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+
+
+class ARiUser(models.Model):
+    """User profile.  Contains some basic configurable settings"""
+    user = models.ForeignKey(User, unique=True)
