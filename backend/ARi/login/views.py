bdb1ed7e1cf14a4083afb526007da9467ecfcdb5
Ruhi Choudhury <arc13@ic.ac.uk>
2017-06-07 13:01:13 +0100

    [RC] Added crsf_exempt annotation to login_user in views.py.

--- backend/ARi/login/views.py
+++ backend/ARi/login/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponseForbidden, JsonResponse
 from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework_jwt.settings import api_settings
 
 from login.models import ARiProfile
 
 
+@csrf_exempt
 def login_user(request):
     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
@@ -19,7 +21,6 @@ def login_user(request):
             ARiProfile.objects.get_or_create(user=user)
 
             payload = jwt_payload_handler(user)
-
             return JsonResponse({'token': jwt_encode_handler(payload)})
         else:
             return HttpResponseForbidden("Disabled user")
