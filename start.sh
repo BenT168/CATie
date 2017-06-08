e70ac9d164f12d6013e7f38389565df4e07df0af
Ruhi Choudhury <arc13@ic.ac.uk>
2017-06-08 11:55:34 +0100

    Correct location of wsgi.py in start.sh.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec gunicorn --pythonpath ARi ARi.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn --pythonpath backend/ARi ARi.wsgi:application --bind 0.0.0.0:8000 --workers 3
