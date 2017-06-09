1703128057652de4d7d0396207aef7a935698574
Harry Uglow <hu115@ic.ac.uk>
2017-06-09 02:40:01 +0100

    A users year and subscribed courses are now found on login (for some courses).

--- backend/ARi/login/models.py
+++ backend/ARi/login/models.py
@@ -2,9 +2,11 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import User
-from courses.models import Year
+
+from courses.models import Year, Course
 
 
 class ARiProfile(models.Model):
     user = models.OneToOneField(User, unique=True)
-    yearGroup = models.ForeignKey(Year)
+    year = models.OneToOneField(Year, unique=True)
+    courses = models.ManyToManyField(Course)
