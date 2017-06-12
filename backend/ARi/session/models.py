1e773de3140634f40b5959e5ef55313240771d70
Harry Uglow <hu115@ic.ac.uk>
2017-06-12 14:53:21 +0100

    Changed get_courses to return all courses to staff.

--- backend/ARi/session/models.py
+++ backend/ARi/session/models.py
@@ -4,8 +4,8 @@ from courses.models import Course
 
 
 class Session(models.Model):
+    urlSuffix = models.CharField(max_length=60, primary_key=True)
     name = models.CharField(max_length=60)
-    urlSuffix = models.CharField(max_length=60)
     course = models.ForeignKey(Course)
     video = models.URLField(default="")
     slides = models.URLField(default="")
---
+++ backend/ARi/session/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from session.models import Session
+
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = ('name', 'video', 'slides')
