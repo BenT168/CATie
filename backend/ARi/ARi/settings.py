f188be220d87994b467eba30b12786d69050851f
Harry Uglow <hu115@ic.ac.uk>
2017-06-14 10:25:46 +0100

    Increased refresh time to 50 minutes.

--- backend/ARi/ARi/settings.py
+++ backend/ARi/ARi/settings.py
@@ -195,7 +195,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=600),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
 
