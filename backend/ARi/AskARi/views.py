c51af85b81b79e8a5c195ce804a13c73033bd63d
Harry Uglow <hu115@ic.ac.uk>
2017-06-14 14:46:28 +0100

    Initial implementation of get_question done.

--- backend/ARi/AskARi/views.py
+++ backend/ARi/AskARi/views.py
@@ -1,9 +1,34 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def get_question(request):
-    return None
+from rest_framework_jwt.utils import jwt_decode_handler
+
+from AskARi.models import Question
+from AskARi.serializers import QuestionSerializer
+from courses.models import Course
+from lecture.models import Lecture
+from login.utils import can_access_course
+
+
+def get_question(request, code, lectureURL, q_id):
+    token = request.environ['HTTP_AUTHORIZATION']
+    username = jwt_decode_handler(token)['username']
+    access, resp = can_access_course(User.objects.get(username=username), code)
+    if not access:
+        return resp
+    course = Course.objects.get(code)
+    try:
+        lecture = Lecture.objects.get(urlName=lectureURL, course=course)
+    except Lecture.DoesNotExist:
+        return HttpResponseNotFound('Lecture ' + lectureURL +
+                                    'not found for course ' + code)
+    question = Question.objects.get(onLecture=lecture, id_per_lecture=q_id)
+    serializer = QuestionSerializer(question, many=False)
+
+    return JsonResponse(serializer.data, safe=False)
 
 
 def get_questions(request, code, lectureURL, pg_no):
