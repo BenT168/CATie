05c3b576c387f424619f8299c2f8f2b30d1144e1
Ruhi Choudhury <arc13@ic.ac.uk>
2017-06-14 14:51:22 +0100

    Fixed merge conflict.

--- backend/ARi/AskARi/views.py
+++ backend/ARi/AskARi/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
+from AskARi.models import Question
 
-
-# Create your views here.
 from rest_framework_jwt.utils import jwt_decode_handler
-
 from AskARi.models import Question
 from AskARi.serializers import QuestionSerializer
 from courses.models import Course
@@ -27,17 +25,24 @@ def get_question(request, code, lectureURL, q_id):
                                     'not found for course ' + code)
     question = Question.objects.get(onLecture=lecture, id_per_lecture=q_id)
     serializer = QuestionSerializer(question, many=False)
-
+    
     return JsonResponse(serializer.data, safe=False)
 
+@permission_classes((IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
+def get_questions(request, code, lectureURL, pg_no):
+    questions = Question.objects.all()
+    return None
 
 def get_questions(request, code, lectureURL, pg_no):
     return None
 
-
+@permission_classes((IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
 def get_questions_all(request, pg_no):
     return get_questions(request, None, None, pg_no)
 
-
+@permission_classes((IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
 def get_questions_course(request, code, pg_no):
     return get_questions(request, code, None, pg_no)
