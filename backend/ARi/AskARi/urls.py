10143d834f5d6baec4f0b762bb684759d37159f4
Harry Uglow <hu115@ic.ac.uk>
2017-06-15 14:47:51 +0100

    Refactored AskARi urls.

--- backend/ARi/AskARi/urls.py
+++ backend/ARi/AskARi/urls.py
@@ -2,18 +2,19 @@ from django.conf.urls import url
 
 from AskARi import views
 
+base = r'^AskARi/'
 
 urlpatterns = [
-    url(r'^AskARi/question/create/$', views.create_question),
-    url(r'^AskARi/question/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,'
+    url(base + 'question/create/$', views.create_question),
+    url(base + 'question/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,'
         r'60})/(?P<q_id>[0-9]+)/$', views.get_question),
-    url(r'^AskARi/question/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,'
+    url(base + 'question/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,'
         r'60})/(?P<q_id>[0-9]+)/reply/$', views.post_comment),
-    url(r'^AskARi/$', views.get_questions),
-    url(r'^AskARi/#(?P<pg_no>[0-9]+)$', views.get_questions),
-    url(r'^AskARi/(?P<code>[0-9]{3})/$', views.get_questions),
-    url(r'^AskARi/(?P<code>[0-9]{3})/#(?P<pg_no>[0-9]+)$', views.get_questions),
-    url(r'^AskARi/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,60})/$',
+    url(base + '$', views.get_questions),
+    url(base + '#(?P<pg_no>[0-9]+)$', views.get_questions),
+    url(base + '(?P<code>[0-9]{3})/$', views.get_questions),
+    url(base + '(?P<code>[0-9]{3})/#(?P<pg_no>[0-9]+)$', views.get_questions),
+    url(base + '(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,60})/$',
         views.get_questions),
     url(r'^AskARi/(?P<code>[0-9]{3})/(?P<lectureURL>[a-zA-Z0-9-]{1,60})/#('
         r'?P<pg_no>[0-9]+)$', views.get_questions),
