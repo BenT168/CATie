12785b97d981fcac274198383d3af681aa0beb41
ab6015 <andrada-ioana.bogoi15@imperial.ac.uk>
2017-11-01 22:14:51 +0000

    Grading schema Done

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -14,6 +14,7 @@ import { GradesComponent } from "./grades/grades.component";
 import { AriViewerComponent } from "./ariviewer/ariviewer.component";
 import { AskAriComponent } from "./askari/askari.component";
 import { AssignmentsComponent } from "./assignments/assignments.component";
+import { GradingSchemaComponent } from "./gradingschema/gradingschema.component";
 import { LoginGuard } from "./_guards/login.guard";
 import { AuthGuard } from "./_guards/auth.guard";
 import { LoginComponent } from "./login/login.component";
@@ -31,6 +32,7 @@ export const ROUTES: Routes = [
     { path: 'ariviewer/:courseCode/:lectureName', component: AriViewerComponent, canActivate: [AuthGuard] },
     { path: 'askari', component: AskAriComponent, canActivate: [AuthGuard] },
     { path: 'assignments', component: AssignmentsComponent, canActivate: [AuthGuard] },
+    { path: 'gradingschema', component: GradingSchemaComponent, canActivate: [AuthGuard] },
     { path: 'askariquestion', component: AskAriQuestionComponent, canActivate: [AuthGuard] }
 ];
 
