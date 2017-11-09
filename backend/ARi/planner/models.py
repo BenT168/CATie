4ef9cf00aa81a75490e4fe21dc457af71c15d4ce
jkatsioloudes <jk2714@ic.ac.uk>
2017-11-09 22:37:20 +0000

    get event functionality added.

--- backend/ARi/planner/models.py
+++ backend/ARi/planner/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-# Create your models here.
-
 
 class CalendarEvent(models.Model):
     username = models.CharField(max_length=10)
     title = models.CharField(max_length=60)
     start = models.DateTimeField()
     end = models.DateTimeField()
+    done = models.BooleanField()
     isDraggable = models.BooleanField()
     isResizable = models.BooleanField()
+
+    def __str__(self):
+        return self.title
