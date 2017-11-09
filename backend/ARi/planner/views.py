40419e9ab0fec4d6faa3735d6dc4ef38e5bbb20b
jkatsioloudes <jk2714@ic.ac.uk>
2017-11-09 22:11:37 +0000

    create event added functionality updated.

--- backend/ARi/planner/views.py
+++ backend/ARi/planner/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import CalendarEvent
 from .serializers import CalendarEventSerializer
 from rest_framework import generics
-
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.utils import jwt_decode_handler
 
-from courses.models import Course
-from courses.serializers import CourseSerializer
-from login.models import ARiProfile
-
-from planner.models import CalendarEvent
 
 @csrf_exempt
 @permission_classes((IsAuthenticated,))
@@ -25,46 +20,15 @@ def create_event(request):
 
     title = request.POST.get('title', None)
     start = request.POST.get('start', None)
+    if start is None:
+        start = ""
     end = request.POST.get('end', None)
+    if end is None:
+        end = ""
 
     CalendarEvent.objects.create(title=title, start=start, end=end)
 
     return HttpResponse("Event created successfully.")
-
-
-# returns the courses codes someone is subscribed to for a given date.
-@permission_classes((IsAuthenticated,))
-@authentication_classes((TokenAuthentication,))
-def get_courses_for_date(date):
-    token = request.environ['HTTP_AUTHORIZATION']
-    username = jwt_decode_handler(token)['username']
-    user = User.objects.get(username=username)
-
-    ari_profile = ARiProfile.objects.get(user=user)
-    courses = ari_profile.courses.all().order_by('code')
-    serializer = CourseSerializer(courses, many=True)
-
-    # serializer.
-
-    # TODO find the way to continue from here and get the courses on that date
-    # TODO using the code pass it below and get the time of the course.
-
-
-# returns the datetime of the provided course lectures to schedule them in the planner.
-def get_course_dateTimes(code):
-    course = Course.objects.get(code=code)
-    dateTimes = [course.dateTime_1, course.dateTime_2]
-    return dateTimes
-
-
-# retuns the time of the course to displayed in the planner.
-def get_course_time(code):
-    course = Course.objects.get(code=code)
-    # this is string parsing depending on the format we are using from here https://docs.djangoproject.com/en/1.9/ref/settings/#datetime-input-formats
-    time1 = 1 # TODO
-    time2 = 2 # TODO
-    times = [time1, time2]
-    return times
 
 
 class CalendarEventList(generics.ListCreateAPIView):
