1ca1ce27408297a9ff5da53ff0f10c78118f6b90
jkatsioloudes <jk2714@ic.ac.uk>
2017-12-22 00:17:56 +0200

    Create submissions added

--- backend/ARi/submissions/views.py
+++ backend/ARi/submissions/views.py
@@ -4,8 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.utils import jwt_decode_handler
 from django.contrib.auth.models import User
 from backend.ARi.submissions.models import Submission
-from django.http import JsonResponse
+from backend.ARi.courses.models import Course
 from backend.ARi.submissions.serializers import SubmissionSerializer
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponse
 
 
 @permission_classes((IsAuthenticated,))
@@ -21,3 +22,31 @@ def get_submissions(request):
     serializer = SubmissionSerializer(submissions, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+@permission_classes((IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
+def create_submissions(request):
+    token = request.environ['HTTP_AUTHORIZATION']
+    username = jwt_decode_handler(token)['username']
+    user = User.objects.get(username=username)
+
+    course = request.POST.get('course', None)
+    if course is None:
+        slides = ""
+    try:
+        course = Course.objects.get(code=course)
+    except Course.DoesNotExist:
+        return HttpResponseNotFound("Creating submission for invalid course.")
+
+    title = request.POST.get('title', None)
+    file = request.POST.get('file', None)
+    marked = request.POST.get('marked', None)
+    grade = request.POST.get('grade', None)
+    collected = request.POST.get('collected', None)
+
+    Submission.objects.create(username=user, course=course, title=title,
+                              file=file, marked=marked, grade=grade,
+                              collected=collected)
+
+    return HttpResponse("Lecture created successfully.")
